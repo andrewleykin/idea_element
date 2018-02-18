e636108ad47aa6941b0839b0f777e476ffd35e41
AndrewLeykin <katashi1328@mail.ru>
2018-02-18 20:33:50 +0300

    add content and font

--- build/app/js/app.js
+++ build/app/js/app.js
@@ -1,8 +1,13 @@
+$(document).ready(function () {
+    svg4everybody({});
+});
+// Библиотека wow.js для анимации
+
+(function () {
+	new WOW().init();
+})();
 // Начальная функция
 
 (function(){
 	console.log('app');
 })();
-$(document).ready(function () {
-    svg4everybody({});
-});
